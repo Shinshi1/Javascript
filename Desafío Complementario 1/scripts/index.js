bef8c0fe56c6c140965ac1c014f89512de75deec
gabriel hidalgo <gabi_hidalgo1@hotmail.com>
2022-08-23 18:07:23 -0300

    Desafio Entregable 1

---
+++ Desafío Complementario 1/scripts/index.js
@@ -0,0 +1,14 @@
+// FOR
+
+let limite = parseInt(prompt("ingresa cuantos caramelos quieres comprar"))
+let caramelos = 0
+
+debugger
+
+for(caramelos; caramelos < limite ; caramelos++) {
+    console.log('caramelo No.', caramelos)
+    
+}
+console.log('Usted ha comprado', caramelos, 'caramelos')
+
+
