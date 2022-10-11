72a8b8023f7ba615714987c8bc86fa2302d34edd
gabriel hidalgo <gabi_hidalgo1@hotmail.com>
2022-10-11 19:26:55 -0300

    agregamos fetch a nuestro proyecto 'tenemos cards'

---
+++ Desafío Entregable 4/bbdd/listaPostres.js
@@ -0,0 +1,38 @@
+let listaPostres = [
+    {
+    id: 1,
+    nombre: "TORTA DE CHOCOLATE",
+    categoria: "TORTAS", 
+    importe: 4100 
+    },
+    {
+    id: 2,
+    nombre: "TIRAMISÚ",
+    categoria: "TORTAS", 
+    importe: 4100 
+    },
+    {
+    id: 3,
+    nombre: "RICOTA",
+    categoria: "TARTAS", 
+    importe: 2360
+    },
+    {
+    id: 4,
+    nombre: "PASTAFROLA",
+    categoria: "TARTAS", 
+    importe: 1500
+    },
+    {
+    id: 5,
+    nombre:  "LEMON PIE",
+    categoria: "POSTRES", 
+    importe: 3800
+    },
+    {
+    id: 6,
+    nombre:  "BROWNIE CON MOUSSE DE CHOCOLATE",
+    categoria: "POSTRES", 
+    importe: 4300
+    }
+]
